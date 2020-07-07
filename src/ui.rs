916605255e950480b4c4eabb3463779ef85afd9c
Lukas Kalbertodt <lukas.kalbertodt@gmail.com>
2020-07-07 23:52:27 +0200

    Cleanup UI module

--- src/ui.rs
+++ src/ui.rs
@@ -3,7 +3,7 @@ use std::{
     sync::{atomic::{Ordering, AtomicBool}, mpsc::Sender, Arc}, fmt, net::SocketAddr,
 };
 use anyhow::Error;
-use termcolor::{Buffer, BufferWriter, Color, ColorChoice, ColorSpec, WriteColor};
+use termcolor::{BufferWriter, ColorChoice, ColorSpec, WriteColor};
 use crate::config::Command;
 
 
@@ -16,31 +16,6 @@ pub struct Ui {
 
 const PREFIX: &str = "════════";
 
-
-fn bold_blue() -> ColorSpec {
-    let mut out = blue();
-    out.set_intense(true);
-    out
-}
-fn blue() -> ColorSpec {
-    let mut out = ColorSpec::new();
-    out.set_fg(Some(Color::Blue));
-    out.set_intense(true);
-    out
-}
-fn magenta() -> ColorSpec {
-    let mut out = ColorSpec::new();
-    out.set_fg(Some(Color::Magenta));
-    out.set_intense(true);
-    out
-}
-fn icon() -> ColorSpec {
-    let mut out = ColorSpec::new();
-    out.set_fg(Some(Color::Yellow));
-    out.set_intense(true);
-    out
-}
-
 impl Ui {
     pub fn new(errors: Sender<Error>) -> Self {
         Self {
@@ -52,142 +27,54 @@ impl Ui {
     }
 
     pub fn watching(&self, action: &str, paths: &[impl fmt::Display]) {
-        self.print_line(false, |buf| {
-            buf.set_color(&magenta())?;
-            write!(buf, "{}", PREFIX)?;
-            buf.set_color(&icon())?;
-            write!(buf, " […] ")?;
-            buf.set_color(&magenta())?;
-            write!(buf, "watching for '{}': ", action)?;
-
-            const MAX_PATHS: usize = 3;
-            for (i, path) in paths.iter().enumerate() {
-                match i {
-                    0 => write!(buf, "`{}`", path)?,
-                    i if i >= MAX_PATHS => {
-                        writeln!(buf, " and {} more", paths.len() - MAX_PATHS)?;
-                        break;
-                    }
-                    i if i == paths.len() - 1 => writeln!(buf, " and `{}`", path)?,
-                    _ => write!(buf, ", `{}`", path)?,
+        let mut paths_str = String::new();
+        const MAX_PATHS: usize = 3;
+        for (i, path) in paths.iter().enumerate() {
+            match i {
+                0 => paths_str = format!("`{}`", path),
+                i if i >= MAX_PATHS => {
+                    paths_str += &format!(" and {} more", paths.len() - MAX_PATHS);
+                    break;
                 }
+                i if i == paths.len() - 1 => paths_str +=  &format!(" and `{}`", path),
+                _ => paths_str += &format!(", `{}`", path),
             }
+        }
 
-            Ok(())
-        });
+        Message::service("👁 ", format!("watching for '{}': {}", action, paths_str)).emit(self);
     }
 
     pub fn listening(&self, addr: &SocketAddr) {
-        self.print_line(false, |buf| {
-            buf.set_color(&magenta())?;
-            write!(buf, "{}", PREFIX)?;
-            buf.set_color(&icon())?;
-            write!(buf, " […] ")?;
-            buf.set_color(&magenta())?;
-            writeln!(buf, "listening on 'http://{}'", addr)?;
-
-            Ok(())
-        });
+        Message::service("👂", format!("listening on 'http://{}'", addr)).emit(self);
     }
 
     pub fn listening_ws(&self, addr: &SocketAddr) {
-        self.print_line(false, |buf| {
-            buf.set_color(&magenta())?;
-            write!(buf, "{}", PREFIX)?;
-            buf.set_color(&icon())?;
-            write!(buf, " […] ")?;
-            buf.set_color(&magenta())?;
-            writeln!(buf, "websockets listening on 'ws://{}'", addr)?;
-
-            Ok(())
-        });
+        Message::service("👂", format!("websockets listening on 'ws://{}'", addr)).emit(self);
     }
 
     pub fn exiting_no_watcher(&self) {
-        self.print_line(false, |buf| {
-            buf.set_color(&bold_blue())?;
-            write!(buf, "{}", PREFIX)?;
-            buf.set_color(&icon())?;
-            write!(buf, " […] ")?;
-            buf.set_color(&bold_blue())?;
-            writeln!(buf, "no HTTP server or watcher configured: we are done already! Bye :)")?;
-
-            Ok(())
-        });
+        Message::status("👋", "no HTTP server or watcher configured: we are done already! Bye :)")
+            .emit(self);
     }
 
     pub fn change_detected(&self, action: &str) {
-        self.print_line(false, |buf| {
-            buf.set_color(&bold_blue())?;
-            write!(buf, "{}", PREFIX)?;
-            buf.set_color(&icon())?;
-            write!(buf, " […] ")?;
-            buf.set_color(&bold_blue())?;
-            write!(buf, "change detected for action '{}', debouncing...", action)?;
-
-            Ok(())
-        });
+        // 📸 🔔 🔥 💧 ⚡ ❄ 🌊 🌈 🌀 ⏳ ⌛ 💡
+        let msg = format!("change detected for action '{}', debouncing...", action);
+        Message::status("⏳", msg)
+            .without_line_ending()
+            .emit(self);
     }
 
     pub fn run_on_change_handlers(&self, action: &str) {
-        self.print_line(true, |buf| {
-            buf.set_color(&bold_blue())?;
-            write!(buf, "{}", PREFIX)?;
-            buf.set_color(&icon())?;
-            write!(buf, " [·] ")?;
-            buf.set_color(&bold_blue())?;
-            writeln!(
-                buf,
-                "change detected for action '{}', executing 'on_change' handler...",
-                action,
-            )?;
-
-            Ok(())
-        });
+        let msg = format!("change detected for action '{}', executing handler...", action);
+        Message::status("🔥", msg)
+            .replace_previous()
+            .emit(self);
     }
 
     pub fn run_command(&self, handler: &str, command: &Command) {
-        self.print_line(false, |buf| {
-            buf.set_color(&blue())?;
-            write!(buf, "{}", PREFIX)?;
-            buf.set_color(&icon())?;
-            write!(buf, " [▶] ")?;
-            buf.set_color(&blue())?;
-            writeln!(buf, "running ({}): {}", handler, command)?;
-
-            Ok(())
-        });
-    }
-
-    fn print_line(
-        &self,
-        replace_line: bool,
-        f: impl FnOnce(&mut Buffer) -> Result<(), Error>,
-    ) {
-        let mut buf = self.writer.buffer();
-
-        // TODO: use try-catch syntax here if it ever gets stabilized.
-        let result = (|| -> Result<(), Error> {
-            if !replace_line && !self.at_start_of_line.load(Ordering::SeqCst) {
-                writeln!(buf)?;
-            }
-            if replace_line {
-                write!(buf, "\r")?;
-            }
-
-            f(&mut buf)?;
-            buf.reset()?;
-            self.writer.print(&buf)?;
-            io::stdout().flush()?;
-
-            Ok(())
-        })();
-
-        if let Err(e) = result {
-            // We ignore the error: if the channel is dropped, all threads will
-            // soon be removed anyway.
-            let _ = self.errors.send(e);
-        }
+        let msg = format!("running ({}): {}", handler, command);
+        Message::status("🐇", msg).emit(self);
     }
 }
 
@@ -196,3 +83,132 @@ impl Ui {
 // ════════════╡ Peter
 // ━━━┝ ┣ ┫  ┥
 // ━━━━━━━━━━━━┥ Peter
+
+struct Message {
+    kind: MessageKind,
+    icon: String,
+    msg: String,
+    end_line: bool,
+    replace_previous: bool,
+}
+
+impl Message {
+    fn new(kind: MessageKind, icon: impl Into<String>, msg: impl Into<String>) -> Self {
+        Self {
+            kind,
+            icon: icon.into(),
+            msg: msg.into(),
+            end_line: true,
+            replace_previous: false,
+        }
+    }
+
+    fn service(icon: impl Into<String>, msg: impl Into<String>) -> Self {
+        Self::new(MessageKind::Service, icon, msg)
+    }
+
+    fn status(icon: impl Into<String>, msg: impl Into<String>) -> Self {
+        Self::new(MessageKind::Status, icon, msg)
+    }
+
+    fn without_line_ending(self) -> Self {
+        Self {
+            end_line: false,
+            ..self
+        }
+    }
+
+    fn replace_previous(self) -> Self {
+        Self {
+            replace_previous: true,
+            ..self
+        }
+    }
+
+    fn emit(&self, ui: &Ui) {
+        let run = || -> Result<(), Error> {
+            let mut buf = ui.writer.buffer();
+
+            if !self.replace_previous && !ui.at_start_of_line.load(Ordering::SeqCst) {
+                writeln!(buf)?;
+            } else if self.replace_previous {
+                write!(buf, "\r")?;
+            }
+
+            buf.set_color(&colors::prefix())?;
+            write!(buf, "{}", PREFIX)?;
+            buf.set_color(&colors::icon())?;
+            write!(buf, " {} ", self.icon)?;
+            buf.set_color(&self.kind.msg_color())?;
+            write!(buf, "{}", self.msg)?;
+
+            if self.end_line {
+                writeln!(buf)?;
+            }
+
+            buf.reset()?;
+            ui.writer.print(&buf)?;
+            io::stdout().flush()?;
+
+            Ok(())
+        };
+
+        if let Err(e) = run() {
+            // We ignore the error: if the channel is dropped, all threads will
+            // soon be removed anyway.
+            let _ = ui.errors.send(e);
+        }
+    }
+}
+
+enum MessageKind {
+    /// A message about a service, usually about starting one. E.g. starting the
+    /// HTTP server or starting to listen.
+    Service,
+
+    /// General messages about some kind of status.
+    Status,
+}
+
+impl MessageKind {
+    fn msg_color(&self) -> ColorSpec {
+        match self {
+            Self::Service => colors::magenta(),
+            Self::Status => colors::bold_blue(),
+        }
+    }
+}
+
+mod colors {
+    use termcolor::{Color, ColorSpec};
+
+    pub fn bold_blue() -> ColorSpec {
+        let mut out = blue();
+        out.set_intense(true);
+        out
+    }
+    pub fn blue() -> ColorSpec {
+        let mut out = ColorSpec::new();
+        out.set_fg(Some(Color::Blue));
+        out.set_intense(true);
+        out
+    }
+    pub fn magenta() -> ColorSpec {
+        let mut out = ColorSpec::new();
+        out.set_fg(Some(Color::Magenta));
+        out.set_intense(true);
+        out
+    }
+    pub fn icon() -> ColorSpec {
+        let mut out = ColorSpec::new();
+        out.set_fg(Some(Color::Yellow));
+        out.set_intense(true);
+        out
+    }
+    pub fn prefix() -> ColorSpec {
+        let mut out = ColorSpec::new();
+        out.set_fg(Some(Color::Green));
+        out.set_intense(true);
+        out
+    }
+}
